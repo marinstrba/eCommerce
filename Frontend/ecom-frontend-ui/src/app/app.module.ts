7e43b1bc9aa3e16864b0cdb28259b01b47442c51
marinstrba <marinstrba@gmail.com>
2024-02-06 12:05:34 +0100

    component in angular creation.

--- Frontend/ecom-frontend-ui/src/app/app.module.ts
+++ Frontend/ecom-frontend-ui/src/app/app.module.ts
@@ -7,6 +7,14 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { NewsLetterComponent } from './footer/news-letter/news-letter.component';
+import { HeroComponent } from './home/hero/hero.component';
+import { FeaturesComponent } from './home/features/features.component';
+import { ServicesComponent } from './home/services/services.component';
+import { BannerComponent } from './home/banner/banner.component';
+import { FailComponent } from './fail/fail.component';
+import { ShopComponent } from './shop/shop.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +22,15 @@ import { NewsLetterComponent } from './footer/news-letter/news-letter.component'
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    NewsLetterComponent
+    NewsLetterComponent,
+    HeroComponent,
+    FeaturesComponent,
+    ServicesComponent,
+    BannerComponent,
+    FailComponent,
+    ShopComponent,
+    UserProfileComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
