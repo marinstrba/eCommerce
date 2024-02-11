7364f6b4fac2c872d5afb30799b628b428ce593a
marinstrba <marinstrba@gmail.com>
2024-02-11 20:52:26 +0100

    almost complete shop with items.

--- Frontend/ecom-frontend-ui/src/app/shop/item/item-details/item-details.component.ts
+++ Frontend/ecom-frontend-ui/src/app/shop/item/item-details/item-details.component.ts
@@ -1,13 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemService } from './item.service';
 import { ShopService } from '../../shop.service';
+import { ProductDTO } from '../../ProductDTO';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-item-details',
   templateUrl: './item-details.component.html',
-  styleUrl: './item-details.component.css'
+  styleUrls: ['./item-details.component.css']
 })
 export class ItemDetailsComponent implements OnInit {
+
+  productName: string | null = null;
+  item!: ProductDTO;
+  relatedItems: Array<ProductDTO> = [];
+
+  categoryCounts: { [category: string]: number } = {};
+  categories = ['MMA', 'Box', 'Wrestling', 'BJJ', 'Judo'];
+
+  constructor (private itemService: ItemService,
+              private route: ActivatedRoute,
+              private shopService: ShopService) {}
   ngOnInit(): void {
+
+     this.categories.forEach(category => {
+       this.shopService.countByCategory(category).subscribe(
+         count => this.categoryCounts[category] = count
+       );
+     });
+
+      this.productName = this.route.snapshot.paramMap.get('productName');
+
+    if (this.productName) {
+      this.itemService.getProduct(this.productName).subscribe(data => {
+        this.item = data;
+        if (this.item.productCategory) {
+          this.shopService.getProductByCategory(this.item.productCategory).subscribe(relatedData => {
+            this.relatedItems = relatedData.filter(relatedItem => relatedItem.productName !== this.productName);
+          });
+        }
+      });
+    }
+
+
 
   }
 }
--- Frontend/ecom-frontend-ui/src/app/shop/item/item-details/item.service.ts
+++ Frontend/ecom-frontend-ui/src/app/shop/item/item-details/item.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ProductDTO } from '../../ProductDTO';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getProduct(name: string): Observable<ProductDTO> {
+    return this.httpClient.get<ProductDTO>(`http://localhost:8080/product/get/${name}`);
+  }
+
+
 }
